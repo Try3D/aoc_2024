8e9776337e7a3b170800ebe2a649e03024455f87
Try3D <rsarans186@gmail.com>
2024-12-04 12:09:28 +0530

    More cleanup

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
 export * as io from "./io";
 export * as math from "./math";
-export * as collections from "./collections";
 export * as matrix from "./matrix";
+export * as collections from "./collections";
