ee8306818295ef46e10d5c509e37c6b21e0cd76f
Try3D <rsarans186@gmail.com>
2025-01-13 14:07:29 +0530

    Day 3

---
+++ 03/part1.ts
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+fs.readFile(
+  "input.txt",
+  "utf8",
+  function (err: NodeJS.ErrnoException | null, data: string) {
+    if (err) throw err;
+
+    const lines = data.trim().split("\n").join("");
+
+    let total = 0;
+    for (let match of lines.matchAll(/mul\((\d+),(\d+)\)/g)) {
+      total += parseInt(match[2]) * parseFloat(match[1]);
+    }
+
+    console.log(total);
+  },
+);
---
+++ 03/part2.ts
@@ -0,0 +1,27 @@
+var fs = require("fs");
+
+fs.readFile(
+  "input.txt",
+  "utf8",
+  function (err: NodeJS.ErrnoException | null, data: string) {
+    if (err) throw err;
+
+    const lines = data.trim().split("\n").join("");
+
+    const re = /(mul\((\d+),(\d+)\))|do\(\)|don't\(\)/g;
+    let total = 0;
+
+    let isAllowed = true;
+    for (let match of lines.matchAll(re)) {
+      if (match[0] == "do()") {
+        isAllowed = true;
+      } else if (match[0] == "don't()") {
+        isAllowed = false;
+      } else if (isAllowed) {
+        total += Number(match[2]) * Number(match[3]);
+      }
+    }
+
+    console.log(total);
+  },
+);
