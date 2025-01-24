e4c22b755cace6c1ea8490ab9f84594e4dbc344b
Try3D <rsarans186@gmail.com>
2025-01-24 08:18:35 +0530

    day 13 not done

---
+++ 13/part1.js
@@ -0,0 +1,39 @@
+// @ts-check
+import fs from "fs";
+
+/** @typedef {{x: number, y: number}} Coordinate */
+
+fs.readFile("input.txt", "utf8", function (err, data) {
+  if (err) throw err;
+
+  const lines = data.trim().split("\n");
+
+  const buttonReg = /Button \w: X\+(\d*), Y\+(\d*)/g;
+  const prizeReg = /Prize: X\=(\d*), Y\=(\d*)/g;
+
+  let total = 0;
+  for (let i = 0; i < lines.length; i += 4) {
+    /** @type {Coordinate} */
+    const btnA = { x: 0, y: 0 };
+    for (let match of lines[i].matchAll(buttonReg)) {
+      btnA.x = Number(match[1]);
+      btnA.y = Number(match[2]);
+    }
+
+    /** @type {Coordinate} */
+    const btnB = { x: 0, y: 0 };
+    for (let match of lines[i + 1].matchAll(buttonReg)) {
+      btnB.x = Number(match[1]);
+      btnB.y = Number(match[2]);
+    }
+
+    /** @type {Coordinate} */
+    const prize = { x: 0, y: 0 };
+    for (let match of lines[i + 2].matchAll(prizeReg)) {
+      prize.x = Number(match[1]);
+      prize.y = Number(match[2]);
+    }
+  }
+
+  console.log(total);
+});
---
+++ 13/part2.js
@@ -0,0 +1,10 @@
+// @ts-check
+import fs from "fs";
+
+/** @typedef {{x: number, y: number}} Coordinate */
+
+fs.readFile("input.txt", "utf8", function (err, data) {
+  if (err) throw err;
+
+  const lines = data.trim().split("\n");
+});
